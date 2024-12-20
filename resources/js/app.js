1572fb4bf1fbd6a4f51d2f8ee09372f9654dacef
Gustavo Freire <gustavo.freire@hfa.mil.br>
2024-12-20 08:40:04 -0300

    Configuração do Ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Ziggy } from '../../ziggy';
 import { InertiaProgress } from '@inertiajs/progress';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -17,19 +18,10 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue, Ziggy)
             .mount(el);
     },
     progress: {
         color: '#4B5563',
     },
 });
-
-
-
-// createInertiaApp({
-//   resolve: name => require(`./Pages/${name}.vue`),
-//   setup({ el, App, props }) {
-//     createApp({ render: () => h(App, props) }).mount(el);
-//   },
-// });
